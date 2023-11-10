7ac5c1e7086a006d4320801fdf233142fc89c9e7
Arduousbook <davids_barretom@javeriana.edu.co>
2023-11-10 06:25:00 -0500

    Partial implementation of login, sign-up

--- app/src/main/java/com/example/taller3_firebase/MainActivity.kt
+++ app/src/main/java/com/example/taller3_firebase/MainActivity.kt
@@ -14,6 +14,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
 
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        // TODO: Update UI or navigate to the next screen
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+
+        }
+         */
     }
+
+    /*
+    private fun saveUserToDatabase(userId: String, firstName: String, lastName: String, idNumber: String, imageUrl: String, latitude: Double, longitude: Double) {
+        val userReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("users").child(userId)
+        val newUser = User(firstName, lastName, idNumber, imageUrl, latitude, longitude)
+        userReference.setValue(newUser)
+    }
+
+     */
 }
--- app/src/main/java/com/example/taller3_firebase/SignInActivity.kt
+++ app/src/main/java/com/example/taller3_firebase/SignInActivity.kt
@@ -1,4 +1,32 @@
 package com.example.taller3_firebase
 
-class SignInActivity {
+import androidx.appcompat.app.AppCompatActivity
+
+class SignInActivity : AppCompatActivity() {
+/*
+    private val auth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        // TODO: Update UI or navigate to the next screen
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+ */
 }
