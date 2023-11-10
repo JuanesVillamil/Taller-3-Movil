75ad57fe084adee17283b283029a435b9baade10
Arduousbook <davids_barretom@javeriana.edu.co>
2023-11-10 12:52:03 -0500

    Validation of emails, communication between screens

--- app/src/main/java/com/example/taller3_firebase/MainActivity.kt
+++ app/src/main/java/com/example/taller3_firebase/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.taller3_firebase
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taller3_firebase.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
@@ -22,6 +26,19 @@ class MainActivity : AppCompatActivity() {
         // Initialize firebase auth
         auth = Firebase.auth
 
+        binding.loginButton.setOnClickListener{
+            val validForm = validateForm()
+
+            if (validForm){
+                val intent = Intent(baseContext, MenuActivity::class.java)
+                startActivity(intent)
+            }
+
+            if (!validForm){
+                Toast.makeText(this@MainActivity, String.format("Form is not valid. Try again."), Toast.LENGTH_SHORT).show()
+            }
+        }
+
         // Add event listener to sign up button
         binding.signupButton.setOnClickListener{
             val intent = Intent(baseContext, SignInActivity::class.java)
@@ -36,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         // Check if there is a user currenty logged in.
         val currentUser = auth.currentUser
 
-        // If there is, launche the menu activity.
+        // If there is, launch the menu activity.
         if (currentUser != null){
             val intent = Intent(baseContext, MainActivity::class.java)
             intent.putExtra("currentUser", currentUser.email)
@@ -47,30 +64,62 @@ class MainActivity : AppCompatActivity() {
         binding.editTextTextEmailAddress.setText("")
         binding.editTextTextPassword.setText("")
 
-
     }
 
-        /*
 
-        loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+    fun validateForm():Boolean{
+        var validEmail: Boolean
+        var validPassword: Boolean
 
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        // TODO: Update UI or navigate to the next screen
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
-
-
+        // Check email
+        val emailString = binding.editTextTextEmailAddress.toString()
+        // First check if its empty.
+        if (TextUtils.isEmpty(emailString)){
+            binding.editTextTextEmailAddress.error = "Required"
+            validEmail = false
+        } else { // Then check if its a valid pattern.
+            validEmail = validateEmail(emailString)
         }
-         */
+
+
+
+        // Check password
+        val passwordString = binding.editTextTextPassword.toString()
+        if (TextUtils.isEmpty(passwordString)){
+            binding.editTextTextPassword.error = "Required"
+            validPassword = false
+        }
+
+        return false
+    }
+
+    fun validateEmail(email: String): Boolean{
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+
+
+    /*
+
+    loginButton.setOnClickListener {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    // TODO: Update UI or navigate to the next screen
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+
+    }
+     */
 
 
     /*
--- app/src/main/java/com/example/taller3_firebase/MenuActivity.kt
+++ app/src/main/java/com/example/taller3_firebase/MenuActivity.kt
@@ -1,4 +1,19 @@
 package com.example.taller3_firebase
 
-class MenuActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.taller3_firebase.databinding.ActivityMainBinding
+import com.example.taller3_firebase.databinding.MenuActivityBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: MenuActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MenuActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
